e8cc35078ed10f6234fc01f5ccfb4a7936cedbfe
Emil Aura <aura.emil@gmail.com>
2015-05-27 22:08:30 +0200

    fixed formats

--- DNM/apps/news/events/forms.py
+++ DNM/apps/news/events/forms.py
@@ -1,18 +1,21 @@
 from DNM.apps.news.forms import *
 from DNM.apps.news.events.models import *
+from functools import partial
+from django.forms import DateTimeInput,DateTimeField
 
 
 
-
+DateTimeInput = partial(DateTimeInput, {'class': 'datepicker'})
 
 class EventForm(NewsForm):
+    date = DateTimeField(input_formats=('%d.%m.%Y %H:%M',),widget=DateTimeInput(format="%d.%m.%Y %H:%M"))
+    deadline = DateTimeField(input_formats=('%d.%m.%Y %H:%M',),widget=DateTimeInput(format="%d.%m.%Y %H:%M"))
+
     class Meta:
         exclude = ('member','madeByAdmin')
         model = Event
         widgets = {
             'description': SummernoteWidget(attrs={ 'width': '100%'}),
-             'date': forms.DateInput(format = "%m/%d/%Y %H:%M",attrs={'class':'datePicker', 'readonly':'true'}),
-             'deadline': forms.DateInput(format = "%m/%d/%Y %H:%M",attrs={'class':'datePicker', 'readonly':'true'}),
 
         }
 
