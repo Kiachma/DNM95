ebe2a4a6279529bc4cb59414ba47bb457b02b376
Emil Aura <aura.emil@gmail.com>
2015-05-28 09:29:51 +0200

    Fixed unicode bug

--- DNM/apps/signup/views.py
+++ DNM/apps/signup/views.py
@@ -11,10 +11,12 @@ from django.contrib.auth import get_user
 from DNM.apps.signup.forms import *
 from DNM.apps.signup.models import *
 from django.forms.formsets import formset_factory
+from django.utils.translation import ugettext as trans
 from datetime import datetime
 from django.utils import timezone
 import pytz
-from django.utils.translation import ugettext as _
+
+
 def save_user_checkboxes(request, event, signup, counter):
     checkboxes = CheckboxXEvent.objects.filter(event__id=event.id)
     # checkboxes= get_check_boxes_for_event(event.id)
@@ -48,15 +50,15 @@ def Save(request, event_id):
     SignUpFormSet = formset_factory(SignUpForm, )
     if request.method == 'POST':
         formset = SignUpFormSet(request.POST, request.FILES)
-        for idx,extraform in enumerate(formset.forms):
-            if idx!=0:
-                extraform.empty_permitted =True
-        if formset.is_valid() :
+        for idx, extraform in enumerate(formset.forms):
+            if idx != 0:
+                extraform.empty_permitted = True
+        if formset.is_valid():
             counter = 0
             for form in formset.forms:
 
                 signup = form.save(commit=False)
-                if(signup.name ==''):
+                if (signup.name == ''):
                     counter = counter + 1
                     continue
                 signup.event = event
@@ -68,11 +70,11 @@ def Save(request, event_id):
                 save_user_checkboxes(request, event, signup, counter)
                 save_user_text_fields(request, event, signup, counter)
 
-            messages.add_message(request, messages.SUCCESS, _(u'Anmälan sparad'))
+            messages.add_message(request, messages.SUCCESS, trans(u'Anmälan sparad'))
             return HttpResponseRedirect(reverse('event:view', args=(event_id,)))
 
     c = {'form': form, 'event': event}
-    messages.add_message(request, messages.ERROR, _(u'Anmälan misslyckades'))
+    messages.add_message(request, messages.ERROR, trans(u'Anmälan misslyckades'))
     return render(request, 'events/view.html', c)
 
 
@@ -86,13 +88,16 @@ def Remove(request, event_id, signup_id):
     signup = SignUp.objects.get(pk=signup_id)
     event = Event.objects.get(pk=event_id)
     u = request.user
-    if (u.title and u.title.styrelsePost) or u.is_superuser or (u==signup.member and event.deadline>datetime.utcnow().replace(tzinfo=pytz.utc)):
+    if (u.title and u.title.styrelsePost) or u.is_superuser or (
+            u == signup.member and event.deadline > datetime.utcnow().replace(tzinfo=pytz.utc)):
         signup.delete()
         delete_extra_fields(signup)
-        messages.add_message(request, messages.SUCCESS, _(u'Anmälan borttagen'))
+        messages.add_message(request, messages.SUCCESS, trans(u'Anmälan borttagen'))
     else:
-        messages.add_message(request, messages.ERROR, _(u'Du har tyvärr inte rättigheter att ta bort denna anmälan, vänligen kontakta styrelsen'))
+        messages.add_message(request, messages.ERROR, trans(u'Du har tyvärr inte rättigheter att ta bort denna anmälan\
+        , vänligen kontakta styrelsen'))
     return HttpResponseRedirect(reverse('event:view', args=(event_id,)))
+
 
 @login_required
 def Edit(request, signup_id):
@@ -100,8 +105,8 @@ def Edit(request, signup_id):
     signup = SignUp.objects.get(pk=signup_id)
     event = signup.event
     if request.method == 'POST':
-        if (u.title and u.title.styrelsePost) or u.is_superuser or (u==signup.member and event.deadline>datetime.utcnow().replace(tzinfo=pytz.utc)):
-
+        if (u.title and u.title.styrelsePost) or u.is_superuser or (
+                u == signup.member and event.deadline > datetime.utcnow().replace(tzinfo=pytz.utc)):
 
             form = SignUpForm(request.POST, instance=signup)
             signup = form.save(commit=False)
@@ -110,10 +115,11 @@ def Edit(request, signup_id):
             delete_extra_fields(signup)
             save_user_checkboxes(request, event, signup, 0)
             save_user_text_fields(request, event, signup, 0)
-            messages.add_message(request, messages.SUCCESS, _(u'Anmälan uppdaterad'))
+            messages.add_message(request, messages.SUCCESS, trans(u'Anmälan uppdaterad'))
             return HttpResponseRedirect(reverse('event:view', args=(signup.event_id,)))
         else:
-            messages.add_message(request, messages.ERROR, _(u'Du har tyvärr inte rättigheter att ta ändra denna anmälan, vänligen kontakta styrelsen'))
+            messages.add_message(request, messages.ERROR, trans(u'Du har tyvärr inte rättigheter att ta ändra denna anmälan,\
+             vänligen kontakta styrelsen'))
             return HttpResponseRedirect(reverse('event:view', args=(event.id,)))
     form = SignUpForm(instance=signup)
     event = signup.event
@@ -122,11 +128,11 @@ def Edit(request, signup_id):
     text_fields = []
     check_boxes = []
     for textfield in event_text_fields:
-        tmp,_ =TextFieldXSignup.objects.get_or_create(textfield_id=textfield.id , signUp_id=signup_id)
+        tmp, _ = TextFieldXSignup.objects.get_or_create(textfield_id=textfield.id, signUp_id=signup_id)
         text_fields.append(tmp)
     for checkbox in event_check_boxes:
-        tmp,_=CheckboxXSignUp.objects.get_or_create(checkbox_id=checkbox.id, signUp_id=signup_id)
+        tmp, _ = CheckboxXSignUp.objects.get_or_create(checkbox_id=checkbox.id, signUp_id=signup_id)
         check_boxes.append(tmp)
 
-    c = {'form': form, 'signup': signup,'check_boxes': check_boxes, 'text_fields': text_fields,}
+    c = {'form': form, 'signup': signup, 'check_boxes': check_boxes, 'text_fields': text_fields, }
     return render(request, 'signup/edit.html', c)
